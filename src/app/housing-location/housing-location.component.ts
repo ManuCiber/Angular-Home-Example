588550b4487f95230add0730a6d84ab7cd37d077
ManuCiber <carlosgameloft22@gmail.com>
2025-02-03 17:06:14 -0400

    First Commit: Angular Project-Home-Tutorial

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: `
+   <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styles: ['./home.component.css']
+})
+export class HousingLocationComponent {
+
+}
